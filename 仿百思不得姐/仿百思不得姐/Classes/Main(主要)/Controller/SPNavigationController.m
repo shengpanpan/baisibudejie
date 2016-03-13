25b61b54ec7bb21e9b1d7cbd983e3f86d0a42f63
tiantian <@qq.com>
2016-03-13 10:29:59 +0800

    导航控制器设置完毕，全局滑动设置OK

--- 仿百思不得姐/仿百思不得姐/Classes/Main(主要)/Controller/SPNavigationController.m
+++ 仿百思不得姐/仿百思不得姐/Classes/Main(主要)/Controller/SPNavigationController.m
@@ -9,7 +9,46 @@
 #import "SPNavigationController.h"
 #import "UIBarButtonItem+SPItem.h"
 #import "UIImage+originalImage.h"
+@interface SPNavigationController ()<UIGestureRecognizerDelegate>
+
+@end
 @implementation SPNavigationController
+
+- (void)viewDidLoad{
+
+    [super viewDidLoad];
+    
+    //设置全局滑动操作
+    [self setAllSlideRight];
+   }
+
+#pragma 设置全局右边滑动操作
+- (void)setAllSlideRight{
+    //设置右边滑动操作
+    
+    /*
+     interactivePopGestureRecognizer<UIScreenEdgePanGestureRecognizer: 0x7f99cac4b790;
+     state = Possible; delaysTouchesBegan = YES;
+     view = <UILayoutContainerView 0x7f99cac4ebd0>;
+     target= <(action=handleNavigationTransition:,
+     target=<_UINavigationInteractiveTransition 0x7f99cac4f9c0>)>>
+     */
+    
+    //创建一个新的手势
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self.interactivePopGestureRecognizer.delegate action:@selector(handleNavigationTransition:)];
+    [self.view addGestureRecognizer:pan];
+    pan.delegate = self;
+    
+    //设置页面左右滑动---手势识别---解决方案，禁止该手势，创建一个新的手势代替。
+    self.interactivePopGestureRecognizer.enabled = NO;
+    
+}
+
+#pragma UIGestureRecognizerDelegate
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer{
+
+    return self.childViewControllers.count > 0;
+}
 
 #pragma 设置导航控制器全局属性
 + (void)load{
@@ -25,7 +64,7 @@
     [navBar setBackgroundImage:[UIImage imageNamed:@"navigationbarBackgroundWhite"] forBarMetrics:UIBarMetricsDefault];
 }
 
-//重写push方法，统一设置返回按钮
+#pragma 重写push方法，统一设置返回按钮
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated{
     
     //控制器跳转之前设置返回按钮
